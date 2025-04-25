62f103e398577111d3b3ac581d1524f2cd3a1084
ernest-jere <ernestmmjere@outlook.com>
2025-04-25 21:18:40 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import {marked} from 'marked';
+
+class MarkdownPreviewer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: '# Markdown Text'
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ markdown: event.target.value });
+  }
+
+  render() {
+    const { markdown } = this.state;
+    const preview = marked(markdown);
+
+    return (
+      <div>
+	<h1 style={{textAlign: 'center'}}>Text Editor</h1>
+        <textarea
+	  id="editor"
+          value={markdown}
+          onChange={this.handleChange}
+          placeholder="Enter Markdown"
+        />
+	<h1 style={{textAlign: 'center'}}>Markdown Previewer</h1>
+        <div
+	  id="preview"
+          dangerouslySetInnerHTML={{ __html: preview }}
+          style={{ border: '1px solid #ddd', padding: '10px', marginTop: '10px', backgroundColor: 'white'}}
+        />
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MarkdownPreviewer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
